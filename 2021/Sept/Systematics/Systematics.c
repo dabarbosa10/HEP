ee5e8c06395199e0027d9c9268d4633396246ce8
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2021-09-23 20:45:54 -0500

    Add files via upload

---
+++ 2021/Sept/Systematics/Systematics.c
@@ -0,0 +1,44 @@
+  TLorentzVector shiftedRecoJet;
+	// Set the new components of the 4-momentum
+  shiftedRecoJet.SetPtEtaPhiM(corrJetPt, recoJet.Eta(), recoJet.Phi(), corrJetMass);
+
+  // std::cout << "I get here in shift particle" << std::endl;
+
+  jet.addP4Syst(shiftedRecoJet, syst);
+  return;
+
+}
+
+void Systematics::shiftLepton(Lepton& lepton, TLorentzVector recoLep, TLorentzVector genLep, double& dPx, double& dPy, int syst){
+  if (genLep == TLorentzVector(0,0,0,0)) {
+    lepton.addP4Syst(recoLep, syst);
+    return;
+  }
+  double ratio = ((genLep.Pt()*scale) + (recoLep.Pt() - genLep.Pt())*resolution)/recoLep.Pt();
+  //cout<<"ratio  "<<ratio<<"  "<<scale<<"  "<<resolution    <<std::endl;
+   //add the shifted part up
+   dPx+=recoLep.Px()*(ratio-1);
+   dPy+=recoLep.Py()*(ratio-1);
+   //WARNING change the particle content for the particle
+   recoLep*=ratio;
+   lepton.addP4Syst(recoLep, syst);
+   return;
+}
+
+
+void Systematics::loadScaleRes(const PartStats& smear, const PartStats& syst, std::string syst_name) {
+  scale = 1;
+  resolution = 1;
+  if(smear.bfind("SmearTheParticle")) {
+    scale = smear.dmap.at("PtScaleOffset");
+    resolution = smear.dmap.at("PtResolutionOffset");
+  }
+  if(syst_name.find("_Res_")) {
+    resolution = syst_name.find("_Up") ? 1 + syst.dmap.at("res") : 1 - syst.dmap.at("res");
+    scale=1;
+  } else if(syst_name.find("_Scale_")) {
+    scale = syst_name.find("_Up") ? 1+syst.dmap.at("scale") : 1- syst.dmap.at("scale");
+    resolution=1;
+  }
+}
+   
